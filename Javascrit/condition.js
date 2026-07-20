b1a6d56843089c4d0c569b78f70e3a3d72f15345
Umasanker-S <umasanker.personal@gmail.com>
2026-07-20 12:30:27 +0530

    first commit

---
+++ Javascrit/condition.js
@@ -0,0 +1,13 @@
+// let age=18;
+// if (age>=18);{
+//     console.log("eligible");
+// }
+
+let age=15;
+if (age>=18){
+    console.log("eligible");
+}jujuju
+else 
+    {
+    console.log("not eligible");
+}
